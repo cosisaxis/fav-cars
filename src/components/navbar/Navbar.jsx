84ce321c0001f397f37aee161e4cd1a88e67fe21
cosisaxis <kishanelewis2@gmail.com>
2022-05-18 21:01:35 -0500

    few-nav-changes

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react'
+import {FaBars, FaTimes, FaFacebook, FaInstagram} from 'react-icons/fa'
+import {GiCarWheel} from 'react-icons/gi'
+import './Navbar.css'
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const navbarHandler = () => setNav(!nav)
+  
+
+
+
+
+
+
+  return (
+    <div className='navbar'>
+        <div className="container">
+            <div className="logo">
+                <h3>Speeder</h3>
+            </div>
+            <ul className={nav? 'navbar-menu active' : 'navbar-menu'}>
+                <li><a href="/">Power</a></li>
+                <li><a href="/">Power</a></li>
+                <li><a href="/">Power</a></li>
+                <li><a href="/">Power</a></li>
+                <li><a href="/">Power</a></li>
+
+                <div className="mobile-menu">
+                    <button>shop</button>
+                    <button>account</button>
+                    <div className="social-media">
+                        <FaFacebook  className='icon'/>
+                        <FaInstagram className='icon'/>
+                        <GiCarWheel className='icon' />
+
+                    </div>
+                </div>
+            </ul>
+            <ul className="navbar-menu disappear">
+                <li><a href="/">Power</a></li>
+                <li><a href="/">Power</a></li>
+            </ul>
+            <div className="hamburg" onClick={navbarHandler}>
+                {nav? (<FaTimes />) : (<FaBars />) }
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
